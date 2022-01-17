76992a9ac92f0283db0cae49c4024cc4f2d56fb6
dexterhuang-blockone <dexter.huang-ext@block.one>
2022-01-17 09:39:33 +0800

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+
+Python 3 Trading Scripts
+--------
+--------
+
+This repository contains Python 3 API trading scripts for [Bullish](https://exchange.bullish.com/) cryptocurrency 
+exchange. This set of scripts can be used out of the box. API traders can use these scripts as a starting point.
+
+Bullish Trading API documentation: https://api.exchange.bullish.com/docs/api/rest/#overview 
+
+## Install dependencies
+
+```
+pip3 install -r requirements.txt
+```
+## Prepare environment variables
+
+Inside the `.env` file, override the default variables with your [JWT token](https://api.exchange.bullish.com/docs/api/rest/#overview--generate-a-jwt-token) 
+and [API key](https://api.exchange.bullish.com/docs/api/rest/#overview--generate-an-api-key):
+````
+export BX_JWT=< your credential >
+
+export BX_PUBLIC_KEY=< your credential >
+export BX_PRIVATE_KEY=< your credential >
+export BX_API_METADATA=< your credential >
+export BX_AUTHORIZER=< your credential >
+````
+
+## Scripts
+
+For a full walk-through on the usage of these scripts, start [here](https://api.exchange.bullish.com/docs/api/rest/#overview--how-to-send-authenticated-requests).  
+
+- Generate a JWT token
+- Create an order
+- Cancel an order
+- Read orders
---
+++ cancel_order.py
@@ -0,0 +1,55 @@
+import json
+import os
+from datetime import datetime, timezone
+from hashlib import sha256
+
+import requests
+from eosio_signer import EOSIOKey
+
+HOST_NAME = os.getenv("BX_API_HOSTNAME")
+PRIVATE_KEY = os.getenv("BX_PRIVATE_KEY")
+JWT_TOKEN = os.getenv("BX_JWT")
+AUTHORIZER = os.getenv("BX_AUTHORIZER")
+
+ORDER_ID = "399014193334321153"
+SYMBOL = "BTCUSD"
+session = requests.Session()
+
+response = session.get(HOST_NAME + "/trading-api/v1/nonce")
+nonce = json.loads(response.text)["lowerBound"]
+next_nonce = str(nonce + 1)
+timestamp = str(int(datetime.now(timezone.utc).timestamp() * 1000))
+
+body = {
+    "timestamp": timestamp,
+    "nonce": next_nonce,
+    "authorizer": AUTHORIZER,
+    "command": {
+        "commandType": "V1CancelOrder",
+        "orderId": ORDER_ID,
+        "handle": None,
+        "symbol": SYMBOL,
+    },
+}
+
+payload = (json.dumps(body, separators=(",", ":"))).encode("utf-8")
+digest = sha256(payload.rstrip()).hexdigest()
+eos_key = EOSIOKey(PRIVATE_KEY)
+signature = eos_key.sign(digest)
+
+headers = {
+    "Content-type": "application/json",
+    "Authorization": f"Bearer {JWT_TOKEN}",
+    "BX-SIGNATURE": signature,
+    "BX-TIMESTAMP": timestamp,
+    "BX-NONCE": next_nonce,
+}
+
+response = session.delete(
+    HOST_NAME
+    + "/trading-api/v1/orders"
+    + f"?orderId={ORDER_ID}"
+    + f"&symbol={SYMBOL}",
+    headers=headers,
+)
+print(f"HTTP Status: {response.status_code}, \n{response.text}")
