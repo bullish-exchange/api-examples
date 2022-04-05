51d5a4b183c9944669991d64ff435b7a332c5a87
dexterhuang-blockone <dexter.huang-ext@block.one>
2022-04-05 10:07:22 +0800

    revert trading-ws routes

--- market_data_web_socket.py
+++ market_data_web_socket.py
@@ -55,7 +55,7 @@ def on_open(conn):
 
 
 def open_connection():
-    ws_conn = websocket.WebSocketApp(HOST_NAME + "/trading-api/v1/market-data",
+    ws_conn = websocket.WebSocketApp(HOST_NAME + "/v1/market-data",
                                      on_open=on_open,
                                      on_message=on_message,
                                      on_error=on_error,
