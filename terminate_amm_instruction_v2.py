6085f4a05545c101f4687fe37b19f75dfb523d34
ji.cao <ji.cao@bullish.com>
2023-03-21 10:49:44 +0800

    v2 model sub account change for create order/ cancel order/ add amm/ remove amm

---
+++ terminate_amm_instruction_v2.py
@@ -0,0 +1,57 @@
+import json
+import os
+from datetime import datetime, timezone
+from hashlib import sha256
+
+import requests
+from eosio_signer import EOSIOKey
+
+HOST_NAME = os.getenv("BX_API_HOSTNAME")
+PRIVATE_KEY = os.getenv("BX_PRIVATE_KEY")
+JWT_TOKEN = os.getenv("BX_JWT")
+AUTHORIZER = os.getenv("BX_AUTHORIZER")
+
+LIQUIDITY_ID = "557839859386417160"
+TRADING_ACCOUNT_ID="111632048797005"
+SYMBOL = "BTCUSD"
+session = requests.Session()
+
+response = session.get(HOST_NAME + "/trading-api/v1/nonce")
+nonce = json.loads(response.text)["lowerBound"]
+next_nonce = str(nonce + 1)
+timestamp = str(int(datetime.now(timezone.utc).timestamp() * 1000))
+
+body = {
+    "timestamp": timestamp,
+    "nonce": next_nonce,
+    "authorizer": AUTHORIZER,
+    "command": {
+        "commandType": "V2RemoveLiquidity",
+        "liquidityId": LIQUIDITY_ID,
+        "symbol": SYMBOL,
+        "tradingAccountId": TRADING_ACCOUNT_ID,
+    },
+}
+
+payload = (json.dumps(body, separators=(",", ":"))).encode("utf-8")
+digest = sha256(payload.rstrip()).hexdigest()
+eos_key = EOSIOKey(PRIVATE_KEY)
+signature = eos_key.sign(digest)
+
+headers = {
+    "Content-type": "application/json",
+    "Authorization": f"Bearer {JWT_TOKEN}",
+    "BX-SIGNATURE": signature,
+    "BX-TIMESTAMP": timestamp,
+    "BX-NONCE": next_nonce,
+}
+
+response = session.delete(
+    HOST_NAME
+    + "/trading-api/v1/amm-instructions"
+    + f"?liquidityId={LIQUIDITY_ID}"
+    + f"&tradingAccountId={TRADING_ACCOUNT_ID}"
+    + f"&symbol={SYMBOL}",
+    headers=headers,
+)
+print(f"HTTP Status: {response.status_code}, \n{response.text}")
