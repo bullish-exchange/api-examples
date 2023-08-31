9f534ea1ec3d4f3ea46749cbd231b71c0d1556bf
Alex Martin <alex.martin@bullish.com>
2023-08-31 12:21:43 -0400

    Provide V1 / V2 HMAC Examples

---
+++ create_order_hmac_v2.py
@@ -0,0 +1,54 @@
+import hmac, requests, os, json
+from datetime import datetime
+from dotenv import load_dotenv
+from hashlib import sha256
+
+load_dotenv()
+
+HOST_NAME = os.getenv("BX_API_HOSTNAME")
+SECRET_KEY = os.getenv("BX_SECRET_KEY")
+JWT_TOKEN = os.getenv("BX_JWT")
+AUTHORIZER = os.getenv("BX_AUTHORIZER")
+TRADING_ACCOUNT_ID = os.getenv("BX_TRADING_ACCOUNT_ID")
+
+session = requests.Session()
+response = session.get(HOST_NAME + "/trading-api/v1/nonce", verify=False)
+nonce = json.loads(response.text)["lowerBound"]
+next_nonce = str(nonce + 1)
+timestamp = str(int(datetime.now(timezone.utc).timestamp() * 1000))
+
+body = {
+    "timestamp": timestamp,
+    "nonce": next_nonce,
+    "authorizer": AUTHORIZER,
+    "command": {
+        "commandType": "V2CreateOrder",
+        "handle": None,
+        "symbol": "BTCUSD",
+        "type": "LMT",
+        "side": "BUY",
+        "price": "30071.5000",
+        "stopPrice": None,
+        "quantity": "1.87000000",
+        "timeInForce": "GTC",
+        "allowMargin": False,
+        "tradingAccountId": TRADING_ACCOUNT_ID,
+    }
+}
+
+payload = (json.dumps(body, separators=(",", ":"))).encode("utf-8")
+digest = sha256(payload).hexdigest().encode('utf-8')
+order_signature = hmac.new(SECRET_KEY, digest, sha256).hexdigest()
+
+headers = {
+    "Content-type": "application/json",
+    "Authorization": f"Bearer {JWT_TOKEN}",
+    "BX-SIGNATURE": signature,
+    "BX-TIMESTAMP": timestamp,
+    "BX-NONCE": next_nonce,
+}
+
+response = session.post(
+    HOST_NAME + "/trading-api/v1/orders", json=body, headers=headers
+)
+print(f"HTTP Status: {response.status_code}, \n{response.text}")
---
+++ generate_jwt_hmac.py
@@ -0,0 +1,27 @@
+import hmac, requests, os
+from datetime import datetime, timezone
+from dotenv import load_dotenv
+from hashlib import sha256
+
+
+HOST_NAME = os.getenv("BX_API_HOSTNAME")
+PUBLIC_KEY = os.getenv("BX_PUBLIC_KEY")
+SECRET_KEY = os.getenv("BX_SECRET_KEY")
+
+session = requests.Session()
+nonce = str(1234567890)
+ts = str(int(datetime.now(timezone.utc).timestamp() * 1000))
+path = "/trading-api/v1/users/hmac/login"
+message = ts + nonce + "GET" + "/trading-api/v1/users/hmac/login"
+signature = hmac.new(SECRET_KEY, message.encode("utf-8"), sha256).hexdigest()
+
+headers = {
+    'BX-PUBLIC-KEY': PUBLIC_KEY,
+    'BX-NONCE': nonce,
+    'BX-SIGNATURE': signature,
+    'BX-TIMESTAMP': ts
+}
+url = HOST_NAME + path
+response = session.get(url, headers=headers)
+
+print(f"HTTP Status: {response.status_code}, \n{response.text}")
