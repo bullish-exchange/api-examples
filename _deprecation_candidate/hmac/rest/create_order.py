b92aa0926bfdd4dc1f35e67037415be9f24f36cc
Daniel Koh <Daniel Koh>
2024-04-11 11:04:51 +0800

    Reorganize examples to their use-case

--- _deprecation_candidate/hmac/rest/create_order.py
+++ _deprecation_candidate/hmac/rest/create_order.py
@@ -1,37 +1,14 @@
-import base64
-import json
-import os
+import hmac, requests, os, json
 from datetime import datetime, timezone
-from hashlib import sha256
-import requests
-import hashlib
-from ecdsa import SigningKey
-from ecdsa.util import sigencode_der, sigdecode_der
 from dotenv import load_dotenv
+from hashlib import sha256
 
 load_dotenv()
 
 HOST_NAME = os.getenv("BX_API_HOSTNAME")
+SECRET_KEY = bytes(os.getenv("BX_SECRET_KEY"), 'utf-8')
 JWT_TOKEN = os.getenv("BX_JWT")
 AUTHORIZER = os.getenv("BX_AUTHORIZER")
-
-public_key_pem = """
------BEGIN PUBLIC KEY-----
-MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE3on42czkgbLfobhZnHOw2cvRLPw+
-ouotZAdvUO+BOc0yN9OS5aTplV2By9LT1+SuETeG4zLg7DytS4ct21ZZkA==
------END PUBLIC KEY-----
-"""
-
-private_key_pem = """
------BEGIN PRIVATE KEY-----
-MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgoE/ut6zgIQ2WBenX
-scngA998+4fOr9ISC8DCrHqH342hRANCAATeifjZzOSBst+huFmcc7DZy9Es/D6i
-6i1kB29Q74E5zTI305LlpOmVXYHL0tPX5K4RN4bjMuDsPK1Lhy3bVlmQ
------END PRIVATE KEY-----
-"""
-
-# Decode the PEM-encoded private key
-private_key = SigningKey.from_pem(private_key_pem)
 
 session = requests.Session()
 response = session.get(HOST_NAME + "/trading-api/v1/nonce", verify=False)
@@ -54,12 +31,12 @@ body = {
         "quantity": "1.87000000",
         "timeInForce": "GTC",
         "allowMargin": False,
-    },
+    }
 }
 
-payload = (timestamp + next_nonce + "POST" + "/trading-api/v1/orders" + json.dumps(body, separators=(",", ":"))).encode("utf-8")
-signature = private_key.sign(payload, hashfunc=hashlib.sha256, sigencode=sigencode_der)
-signature = base64.b64encode(signature).decode()
+payload = (json.dumps(body, separators=(",", ":"))).encode("utf-8")
+digest = sha256(payload).hexdigest().encode('utf-8')
+signature = hmac.new(SECRET_KEY, digest, sha256).hexdigest()
 
 headers = {
     "Content-type": "application/json",
